d875d8d15800a08b82392d59041398ba603b4c91
Varna Singh <vkerrysingh@gmail.com>
2019-02-03 07:06:29 +0000

    Implement Authorised users.

--- sol/SimpleCoin.sol
+++ sol/SimpleCoin.sol
@@ -1,59 +1,61 @@
 pragma solidity >=0.4.22 <0.6.0;
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+library Library {
+  struct UserAttributes {
+     uint256 val;
+     bool isAuth;
+   }
+}
+
 contract SimpleCoin is Ownable{
 
+    using Library for Library.UserAttributes;
     uint256 totalAmt;
-    //address owner;
-    mapping(address=>uint256) accountMap;
+    mapping(address=>Library.UserAttributes) accountMap;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Failed(string);
 
-   /*
-    modifier hasBalanceAmt(address _sourceAddress, uint256 _value){
-        if (accountMap[msg.sender] >= _value) {
-            _;
-        }
-        else
-            emit Failed("Failed");
-    }
-    */
-
     constructor() public {
         totalAmt=2000000;
-        accountMap[msg.sender]=totalAmt;
+        accountMap[msg.sender].val=totalAmt;
+        accountMap[msg.sender].isAuth = true;
     }
 
-    function totalSupply() view public returns (uint256) {
+    modifier onlyAuthAccount(address account) {
+        require(accountMap[account].isAuth);
+        _;
+    }
+
+    function totalSupply() view public onlyAuthAccount(msg.sender) returns (uint256) {
         return totalAmt;
     }
 
-
-    function balanceOf(address _owner) view public returns (uint256){
-        return accountMap[_owner];
-    }
-
-    function myBalance() view public returns (uint256){
-        return accountMap[msg.sender];
+    function myBalance() view public onlyAuthAccount(msg.sender) returns (uint256){
+        return accountMap[msg.sender].val;
     }
 
     function symbol() pure public returns(string memory){
         return "VS";
     }
 
-    function transfer(address _to, uint256 _value) public onlyOwner() returns (bool){
-        accountMap[msg.sender] -= _value;
-        accountMap[_to] += _value;
+    function addAuthUser(address _user) public onlyAuthAccount(msg.sender) {
+        accountMap[_user].isAuth = true;
+    }
+
+    function transfer(address _to, uint256 _value) public onlyAuthAccount(msg.sender) returns (bool){
+        accountMap[msg.sender].val -= _value;
+        accountMap[_to].val += _value;
         emit Transfer(msg.sender,_to,_value);
     }
 
-    /*
-    function transfer(address _to, uint256 _value) public hasBalanceAmt(msg.sender,_value) returns (bool){
-        accountMap[msg.sender] -= _value;
-        accountMap[_to] += _value;
-        emit Transfer(msg.sender,_to,_value);
+    //Use of Ownable functions
+    function balanceOfOwner(address _owner) view public onlyOwner() returns (uint256){
+        return accountMap[_owner].val;
     }
-    */
 
+    function transferOwner(address _newOwner) public {
+        transferOwnership(_newOwner);
+    }
 }
